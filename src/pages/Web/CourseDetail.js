a34fbd4fd25f1726672112513337d0339794322c
HanHwiyun <hyyun0430@gmail.com>
2023-10-25 12:15:17 +0900

    23-10-25 과정안내, 소개, 입학절차 페이지 작업 완료

--- src/pages/Web/CourseDetail.js
+++ src/pages/Web/CourseDetail.js
@@ -1,5 +1,92 @@
+import styled from "styled-components"
+import WebWrapper from "../../components/WebWrapper"
+import { Row, Col } from "react-bootstrap";
+
+const Container = styled.div`
+  margin: 2rem 10rem;
+  padding-bottom: 3rem;
+`;
+
+const Card = styled.div`
+  padding: 3rem 5rem;
+  background-color: #ecf5f9;
+  border-radius: 1rem;
+  text-align: center;
+  margin-bottom: 1.3rem;
+`;
+
+const H2 = styled.p`
+  font-size: 2.5rem;
+  font-weight: 700;
+  margin: 10px 0;
+`;
+
+const Divider = styled.div`
+  color: #bbb;
+  margin: 0;
+  padding: 0;
+  cursor: default;
+  font-size: 1.5rem;
+`;
+
+const Box = styled.div`
+  display: flex;
+  gap: 2rem;
+  padding-top: 1rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+const P = styled.p`
+  font-size: 1.5rem;
+  margin: 0;
+`
+
+const TextBox = styled.div`
+  padding: 1.3rem 0;
+`;
+
+const H3 = styled.p`
+  font-size: 2rem;
+  font-weight: 700;
+  margin: 15px 0px;
+`;
+
+const DividerBox = styled.div`
+  width: 40px;
+  height: 4px;
+  background-color: #5f7dcf;
+`;
+
 export function CourseDetail() {
   return <>
-  
+    <WebWrapper pageName={"과정 소개"} />
+    <Container>
+      <Card>
+        <H2>class명</H2>
+        <p className="mb-4 fs-5">HTML, CSS, JS, DB, JAVA</p>
+        <hr />
+        <Box>
+          <P>개강 일자: 2023-10-31</P>
+          <Divider>|</Divider>
+          <P>모집 종료 일자: 2023-11-06</P>
+          <Divider>|</Divider>
+          <P>모집 인원: 30</P>
+        </Box>
+      </Card>
+      <TextBox>
+        <DividerBox />
+        <H3>과정소개</H3>
+        <P>「한국판 뉴딜」사업의 일환으로 혁신적인 기술 및 훈련 방법을 갖춘 기업-대학-민간 혁신기관을 통해 4차 산업혁명과 함께 급성장하고 있는 디지털·신기술 분야에 대한 집중적 교육 훈련을 합니다.<br />
+디지털·신기술 분야 현장에서 기업이 원하는 핵심 실무 인재를 양성하는 것이 목적입니다. <br />
+프로젝트 과제 수행, 해커톤, 기업과제 해결 등을 통한 차별적인 훈련 방법을 추구합니다.<br />
+국민내일배움카드 발급자를 대상으로 훈련비 전액을 지원합니다.<br />
+훈련장려금을 매월 최대 316,000~816,000원을 지원합니다.<br />
+훈련생의 자기주도적 과업수행능력과 종합적인 실무적용능력을 개발하는 것을 목적으로 하는 프로젝트 기반 훈련(PBT, Project Based Training)입니다.<br />
+신기술 분야인 드론 기업의 수요를 반영하고 참여기업의 강사(멘토)가 참여하는 과정입니다.<br />
+대전 소재의 드론 기업과 하드웨어, 소프트웨어 간의 융합 솔루션을 제공하는 기업을 포함, 총 58개 기업의 인력 수요를 파악하여 설계된 과정입니다.<br />
+본 과정에서 필요한 모든 키트, 교재 및 IDE를 무상 제공합니다.</P>
+      </TextBox>
+    </Container>
   </>
 }
--- src/pages/Web/Step.js
+++ src/pages/Web/Step.js
@@ -1,12 +1,182 @@
 import styled from "styled-components"
-import { Container } from "react-bootstrap";
 import WebWrapper from "../../components/WebWrapper"
+import { Col, Row } from "react-bootstrap";
+
+const Container = styled.div`
+  margin: 2rem;
+  margin-left: 3rem;
+  padding-bottom: 3rem;
+`;
+
+const TitleBox = styled.div`
+  padding: 3rem 5rem;
+  background-color: #ecf5f9;
+  border-radius: 1rem;
+  text-align: center;
+  margin-bottom: 1.3rem;
+`;
+
+const H2 = styled.p`
+  font-size: 2.5rem;
+  font-weight: 700;
+  margin: 15px 0px;
+`;
+
+const H3 = styled.p`
+  font-size: 2rem;
+  font-weight: 700;
+  margin: 15px 0px;
+`;
+
+const TextBox = styled.div`
+  padding: 1.3rem 0;
+`;
+
+const Li = styled.li`
+  font-size: 1.6rem;
+  line-height: 2rem;
+`;
+
+const Divider = styled.div`
+  width: 40px;
+  height: 4px;
+  background-color: #5f7dcf;
+`;
+
+const Box = styled.div`
+  background: #ecf5f9;
+  padding: 15px 20px;
+  margin-bottom: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 1rem;
+`;
+
+const H5 = styled.div`
+  font-size: 1.1rem;
+  font-weight: 500;
+  &.main{
+    font-weight: bold;
+    text-align: end;
+    margin-bottom: 1.5rem;
+    font-size: 1.5rem;
+  }
+`;
 
 export function Step() {
   return <>
     <WebWrapper pageName={"입학 절차"} />
     <Container>
-      
+      <TitleBox>
+        <H2>고용 노동부 장관이 실시하는 직업교육</H2>
+        <p classNameName="fs-5">15세이상 취업에 필요한 기능과 기술을 습득하기 위한 교육 기회를 제공하고 재취업 및 창업에 필요한 직업능력개발을 위하여 고용 노동부 장관이 실시하는 직업교육입니다.</p>
+      </TitleBox>
+      <TextBox>
+        <Divider />
+        <H3>신청자격</H3>
+        <ul>
+          <Li>직업안정기관에 구직 등록한 15세 이상 실업자</Li>
+          <Li>고등학교 3학년 재학생으로서 대학진학 예정이 없는 학생</Li>
+          <Li>대학(전문대학) 최종학년 재학생으로, 대학원등 진학하지 아니하며, 다음연도 2월말까지 졸업예정자</Li>
+          <Li>야간대학교, 사이버대학교, 방송통신대학 재학생(휴학생은 제외)</Li>
+          <Li>사업기간이 1년이상이면서 연간 매출액이 8천만원 미만인 영세자영업자, 학습지교사, 골프장 경기보조원, 보험설계사, 택배기사, 퀵서비스기사</Li>
+        </ul>
+      </TextBox>
+      <TextBox>
+        <Divider />
+        <H3>국비지원내용</H3>
+        <ul>
+          <Li>교육비 및 교재비100% 국비전액무료</Li>
+          <Li>훈련비 훈련장려금지급( 교통비, 식대포함 11만 6천원 지급)</Li>
+          <Li>유형 및 개인의 상황에 따라 훈련장려금은 상이할 수 있습니다.</Li>
+        </ul>
+      </TextBox>
+      <TextBox>
+        <Divider />
+        <H3>지원내용</H3>
+        <ul>
+          <Li>756여개의 산학협동협약체결 된 업체와의 동행 면접 시행</Li>
+          <Li>수료생 전원 100% 취업 추천 및 취업 관리</Li>
+          <Li>관련 자격증 취득시 까지 교육지원</Li>
+        </ul>
+      </TextBox>
+      <TextBox>
+        <H3>교육신청 절차</H3>
+        <Row>
+          <Col>
+            <Box>
+              <H5>Step1</H5>
+              <div>
+                <H5 className="main">워크넷</H5>
+                <H5>구직신청(인증)</H5>
+              </div>
+            </Box>
+            <Box>
+              <H5>Step4</H5>
+              <div>
+                <H5 className="main">훈련상담</H5>
+                <H5>자기탐생활동 /
+                  훈련정보 수집</H5>
+              </div>
+            </Box>
+            <Box>
+              <H5>Step7</H5>
+              <div>
+                <H5 className="main">훈련기관 방문</H5>
+                <H5>적합 훈련 과정 수강신청</H5>
+              </div>
+            </Box>
+          </Col>
+          <Col>
+            <Box>
+              <H5>Step2</H5>
+              <div>
+                <H5 className="main">HRD-Net</H5>
+                <H5>교육동영상 시청 /
+                    훈련과정 탐색</H5>
+              </div>
+            </Box>
+            <Box>
+              <H5>Step5</H5>
+              <div>
+                <H5 className="main">훈련 필요성 인정</H5>
+                <H5>개인 훈련 계획 수립 및 작성</H5>
+              </div>
+            </Box>
+            <Box>
+              <H5>Step8</H5>
+              <div>
+                <H5 className="main">수강</H5>
+                <H5>훈련과정 수강</H5>
+              </div>
+            </Box>
+          </Col>
+          <Col>
+            <Box>
+              <H5>Step3</H5>
+              <div>
+                <H5 className="main">고용센터 방문</H5>
+                <H5 className="text-end">계좌발급 신청</H5>
+              </div>
+            </Box>
+            <Box>
+              <H5>Step6</H5>
+              <div>
+                <H5 className="main">내일배움카드</H5>
+                <H5>내일배움카드 발급 및 수령</H5>
+              </div>
+            </Box>
+            <Box>
+              <H5>Step9</H5>
+              <div>
+                <H5 className="main">HRD-Net</H5>
+                <H5>수강평 입력</H5>
+              </div>
+            </Box>
+          </Col>
+        </Row>
+      </TextBox>
     </Container>
   </>
 }
