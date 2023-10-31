0b06e00f8d007b8cd01fb530711042abc1a8584f
parksungyun <ayame0102@gmail.com>
2023-10-31 15:52:03 +0900

    2023-10-31 파일 생성

---
+++ src/pages/Learning/AdminManagerDetail.js
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom";
+import { academic } from "../../assets/TempData";
+
+export function AdminManagerDetail() {
+  const { id } = useParams();
+
+  return <>
+  
+  </>
+}
