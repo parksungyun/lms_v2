c28041086c55de65a8bea693abce863cfc925dda
HanHwiyun <hyyun0430@gmail.com>
2023-11-06 15:56:34 +0900

    23-11-06 컴포넌트 및 페이지 생성

---
+++ src/pages/Learning/TrainerLectureWrite.js
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 1.5rem 2rem;
+  padding-bottom: 2rem;
+  background-color: #f6f9ff;
+  height: 100%;
+`;
+
+const TableBox = styled.div`
+  padding: 2rem;
+  padding-top: 1.3rem;
+  background-color: white;
+  border-radius: 1rem;
+  margin-bottom: 2rem;
+`;
+
+const H2 = styled.p`
+  font-size: 1.7rem;
+  font-weight: bold;
+  margin-top: 10px;
+  margin-bottom: 0.5rem;
+`;
+
+const Hr = styled.hr`
+  border: 0 solid #ddd;
+`;
+
+const PrimaryButton = styled.button`
+  border: 0;
+  border-radius: 5px;
+  background-color: #5f7dcf;
+  padding: 0.6rem 1.4rem;
+  color: white;
+`;
+
+const SecondaryButton = styled.button`
+  border: 0;
+  border-radius: 5px;
+  background-color: gray;
+  padding: 0.6rem 1.4rem;
+  color: white;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.7rem;
+  border: 1px solid lightgray;
+  border-radius: 0.5rem;
+  margin-top: 0.5rem;
+`;
+
+const ContentInput = styled.textarea`
+  width: 100%;
+  height: 300px;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid lightgray;
+  resize: none;
+`;
+
+const Box = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  text-align: center;
+  margin: 0;
+  justify-content: center;
+  margin-top: 1rem;
+  margin-bottom: 0;
+`;
+
+const P = styled.p`
+  margin: 0;
+  margin-top: 1rem;
+  font-size: 1.2rem;
+`
+
+export function TrainerLectureWrite() {
+  const navigate = useNavigate();
+  const [lecture_title, setLecture_title] = useState();
+  const [lecture_content, setLecture_content] = useState();
+  const [lecture_video, setLecture_video] = useState();
+  const [lecture_file, setLecture_file] = useState();
+  return<>
+    <Container>
+      <TableBox>
+        <H2>강의 등록</H2>
+        <form action="" method="POST">
+          <Input type="text" name="lecture_title" id="lecture_title" value={lecture_title} onChange={(e)=>setLecture_title(e.target.value)} placeholder="제목 입력해주세요"/>
+          <Hr />
+          <ContentInput type="text" name="lecture_content" id="lecture_content" value={lecture_content} onChange={(e)=>setLecture_content(e.target.value)} placeholder="제목 입력해주세요"/>
+          <P>영상 강의</P>
+          <Input type="file" name="lecture_video" id="lecture_video" value={lecture_video} onChange={(e)=>setLecture_video(e.target.value)}/>
+          <P>강의 자료</P>
+          <Input type="file" name="lecture_file" id="lecture_file" value={lecture_file} onChange={(e)=>setLecture_file(e.target.value)}/>
+          <Box>
+            <PrimaryButton type="submit">등록</PrimaryButton>
+            <SecondaryButton onClick={() => navigate(-1)}>목록</SecondaryButton>
+          </Box>
+        </form>
+      </TableBox>
+    </Container>
+  </>
+}
